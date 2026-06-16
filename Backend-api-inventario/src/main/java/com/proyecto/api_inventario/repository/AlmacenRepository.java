b66657532bae766ccd0af08734c29b1b7814cd23
LeiderF <leider.caicedo@udea.edu.co>
2026-06-16 00:30:52 -0500

    feat: some files are modified

--- Backend-api-inventario/src/main/java/com/proyecto/api_inventario/repository/AlmacenRepository.java
+++ Backend-api-inventario/src/main/java/com/proyecto/api_inventario/repository/AlmacenRepository.java
@@ -2,6 +2,8 @@ package com.proyecto.api_inventario.repository;
 
 import com.proyecto.api_inventario.models.Almacen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlmacenRepository extends JpaRepository<Almacen, Long> {
 }
